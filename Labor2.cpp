a085efbc8e54729064e19dd691a8304978190dfe
dest316 <90666585+dest316@users.noreply.github.com>
2021-09-28 14:22:33 +1000

    Add files via upload

---
+++ Labor2.cpp
@@ -0,0 +1,36 @@
+﻿// Labor2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+
+class Function
+{
+private:
+    const double a = 11.2;
+    const double b = 10.83;
+    const double c = 9.31;
+    double y = 0;
+    double x = 0;
+    double result;
+public:
+    Function(double x)
+    {
+        if (x < 0) { x = abs(x); }
+        this->x = x;
+        y = (c - log(x)) / b;
+        ((x >= 0) || (abs(x) + abs(y) <= 1)) ? (result = 1) : result = 2;
+    }
+    void printResult()
+    {
+        std::cout << "result = " << result << std::endl;
+    }
+};
+
+int main()
+{
+    Function fun(0);
+    fun.printResult();
+    return 0;
+}
