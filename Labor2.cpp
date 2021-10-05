07ceed7b53f4af85e69058af64fe4e4dc0ce5302
dest316 <90666585+dest316@users.noreply.github.com>
2021-10-05 13:49:11 +1000

    Update Labor2.cpp

--- Labor2.cpp
+++ Labor2.cpp
@@ -19,6 +19,8 @@ public:
     {
         std::cin>>x;
         if (x < 0) { x = abs(x); }
+        if (x == 0) {x = 1; cout << "x был приравнен к 1, так как x = 0 не удовл. ОДЗ"<<endl;}
+        
         this->x = x;
         y = (c - log(x)) / b;
         ((x >= 0) || (abs(x) + abs(y) <= 1)) ? (result = 1) : result = 2;
